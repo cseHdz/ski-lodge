569a0de551e41ec8c62945109fa374802f82978a
Carlos Hernandez <carlos.hdz@me.com>
2018-10-06 15:22:26 -0400

    Implemented a dashboard view

--- app.R
+++ app.R
@@ -3,45 +3,51 @@
 
 library(shiny)
 library(googlesheets)
+library (shinydashboard)
 
 # Import data
-
 gs_data <- gs_key("1-17_HfRFg6guIf3lDs-Lsa6zGmOhKRU5AkNQvUcWnNc", lookup = FALSE)
 data <- gs_read(gs_data)
 
+# Modify Data
 data$day_week <- weekdays(as.Date(data$date))
-
 data<- data[data$season != 0,]
-
 data$DoW <- factor(data$day_week, levels= c("Monday", 
                                          "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"))
 
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Ski Lessons - Mt. Tremblant"),
-   
-   fluidRow(
-     column(width = 2,
-            numericInput('s_week', 'Start week:', 1, 1, 10, 1)),
-     column(width = 2,
-            numericInput('e_week', 'End week:', 1, 1, 10, 1)),
-     column(width = 2,
-            selectInput("season", "Season:", 
-                        choices = data$season[data$season != 0],
-                        selected = data$season[data$season == 1],
-                        multiple = TRUE))
-   ),
-   
-   fluidRow(
-     column(width = 5,
-            plotOutput("avg_lessons"),
-            plotOutput("revenue"),
-            tags$head(tags$style("#avg_lessons{height:25vh !important;}")),
-            tags$head(tags$style("#revenue{height:25vh !important;}")))
-   )
+# Define UI for application 
+ui <- dashboardPage(
+  dashboardHeader(
+    # Application title
+    title = "Ski Lessons - Mt. Tremblant"
+  ),
+  dashboardSidebar(
+    #menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+    selectInput("season", "Season:", 
+                choices = data$season[data$season != 0],
+                selected = data$season[data$season == 0]),
+    numericInput('s_week', 'Start week:', 1, 1, 10, 1),
+    numericInput('e_week', 'End week:', 1, 1, 10, 1)
+  ),
+  dashboardBody(
+
+    fluidRow(
+      column(width = 5,
+             box(
+               title = "Average Lessons Per Day", solidHeader = TRUE, width = NULL, status = "primary",
+               plotOutput("avg_lessons"),
+               tags$head(tags$style("#avg_lessons{height:30vh !important;}"))
+               ),
+             box(
+               title = "Revenue per Day", solidHeader = TRUE, width = NULL, status = "primary",
+               plotOutput("revenue"),
+               tags$head(tags$style("#revenue{height:30vh !important;}"))
+             )
+      )
+    )
+  )
+  
 )
 
 # Define server logic required to draw a histogram
