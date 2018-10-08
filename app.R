6cd2976f9d9b40780c0db3b77708560b7ee4f005
Carlos Hernandez <carlos.hdz@me.com>
2018-10-07 21:54:05 -0400

    Added library and cleaned code

--- app.R
+++ app.R
@@ -4,8 +4,8 @@
 library(shiny)
 library(googlesheets)
 library (shinydashboard)
-library(lubridate)
 library(data.table)
+suppressMessages(library(dplyr))
 
 # Import data
 gs_data <- gs_key("1-17_HfRFg6guIf3lDs-Lsa6zGmOhKRU5AkNQvUcWnNc", lookup = FALSE)
@@ -13,94 +13,93 @@ summary <- gs_read(gs_data, ws = "summary")
 sales_data <- gs_read(gs_data, ws = "sales_data")
 
 # Define UI for application 
-ui <- 
-  
+ui <- (
   dashboardPage(
-  dashboardHeader(
-    # Application title
-    title = "Skiing Lessons"
-  ),
-  dashboardSidebar(
-    # Season Selector
-    selectInput("season", "Season:", 
-                choices = summary$season[summary$season != 0],
-                selected = summary$season[summary$season == 0]),
-    
-    # Dashboard View Selector
-    selectInput("view", "Dashboard View:", 
-                choices = c('This Season', 'by Month','by Weekday'),
-                selected = 'This Season'),
-    sidebarMenu(
+    dashboardHeader(
+      # Application title
+      title = "Skiing Lessons"
+    ),
+    dashboardSidebar(
+      # Season Selector
+      selectInput("season", "Season:", 
+                  choices = summary$season[summary$season != 0],
+                  selected = summary$season[summary$season == 0]),
       
-      # Tabs
-      menuItem("Profit Analysis", tabName = "profit",icon=icon("usd")),
-      menuItem("Capacity Management", tabName = "capacity",icon=icon("users")),
-      menuItem("Weather Assessment", tabName = "weather",icon=icon("snowflake-o")),
-      menuItem("Scenario Builder", tabName = "scenario",icon=icon("area-chart"))
-    ),  
-    
-    # Summary Metrics
-    column(width = 5,style='padding:10px',align = 'center',
-           tableOutput("metrics1"))
-           #tableOutput("metrics2"))
-  ),
-  dashboardBody(
-    tabItems(
+      # Dashboard View Selector
+      selectInput("view", "Dashboard View:", 
+                  choices = c('This Season', 'by Month','by Weekday'),
+                  selected = 'This Season'),
+      sidebarMenu(
+        
+        # Tabs
+        menuItem("Profit Analysis", tabName = "profit",icon=icon("usd")),
+        menuItem("Capacity Management", tabName = "capacity",icon=icon("users")),
+        menuItem("Weather Assessment", tabName = "weather",icon=icon("snowflake-o")),
+        menuItem("Scenario Builder", tabName = "scenario",icon=icon("area-chart"))
+      ),  
       
-      # Individual sections corresponding to the tabs in the tab bar
-      
-      tabItem("profit",
-              #KPI Row
-              fluidRow(
-                valueBoxOutput("total_profit"),
-                valueBoxOutput("avg_profit_lesson"),
-                valueBoxOutput("avg_profit_instructor")
-              ),
-              
-              #Charts Row
-              fluidRow(
-                column(width = 6,
-                       box(title = "Profit Trends", solidHeader = TRUE, width = NULL, status = "primary",
-                           plotOutput("profit_chart"),tags$head(tags$style("#profit_chart{height:25vh !important;}")))),
-                column(width = 6,
-                       box(title = "Profit per Lesson", solidHeader = TRUE, width = NULL, status = "primary",
-                         plotOutput("profit_l_chart"),tags$head(tags$style("#profit_l_chart{height:25vh !important;}"))))),
-              fluidRow(
-                column(width = 6,
-                       box(title = "Revenue Analysis", solidHeader = TRUE, width = NULL, status = "primary",
-                         plotOutput("revenue_chart"),tags$head(tags$style("#revenue_chart{height:25vh !important;}")))),
-                column(width = 6,
-                       box(title = "Cost Analysis", solidHeader = TRUE, width = NULL, status = "primary",
-                         plotOutput("cost_chart"),tags$head(tags$style("#cost_chart{height:25vh !important;}"))))
+      # Summary Metrics
+      column(width = 5,style='padding:10px',align = 'center',
+             tableOutput("metrics1"))
+             #tableOutput("metrics2"))
+    ),
+    dashboardBody(
+      tabItems(
+        
+        # Individual sections corresponding to the tabs in the tab bar
+        tabItem("profit",
+                #KPI Row
+                fluidRow(
+                  valueBoxOutput("total_profit"),
+                  valueBoxOutput("avg_profit_lesson"),
+                  valueBoxOutput("avg_profit_instructor")
+                ),
+                
+                #Charts Row
+                fluidRow(
+                  column(width = 6,
+                         box(title = "Profit Trends", solidHeader = TRUE, width = NULL, status = "primary",
+                             plotOutput("profit_chart"),tags$head(tags$style("#profit_chart{height:25vh !important;}")))),
+                  column(width = 6,
+                         box(title = "Profit per Lesson", solidHeader = TRUE, width = NULL, status = "primary",
+                           plotOutput("profit_l_chart"),tags$head(tags$style("#profit_l_chart{height:25vh !important;}"))))),
+                fluidRow(
+                  column(width = 6,
+                         box(title = "Revenue Analysis", solidHeader = TRUE, width = NULL, status = "primary",
+                           plotOutput("revenue_chart"),tags$head(tags$style("#revenue_chart{height:25vh !important;}")))),
+                  column(width = 6,
+                         box(title = "Cost Analysis", solidHeader = TRUE, width = NULL, status = "primary",
+                           plotOutput("cost_chart"),tags$head(tags$style("#cost_chart{height:25vh !important;}"))))
+                  )
+        ),
+        tabItem("capacity",
+                #KPI Row
+                fluidRow(
+                  valueBoxOutput("lessons_instructor"),
+                  valueBoxOutput("days_overstaffed"),
+                  valueBoxOutput("avg_lessons_day")
+                ),
+                
+                #Charts Row
+                fluidRow(
+                  column(width = 6,
+                         box(title = "Lesson Trends", solidHeader = TRUE, width = NULL, status = "primary",
+                             plotOutput("lessons_chart"),tags$head(tags$style("#lessons_chart{height:25vh !important;}")))),
+                  column(width = 6,
+                         box(title = "Lessons per Instructor", solidHeader = TRUE, width = NULL, status = "primary",
+                             plotOutput("lessons_i_chart"),tags$head(tags$style("#lessons_i_chart{height:25vh !important;}"))))),
+                fluidRow(
+                  column(width = 6,
+                         box(title = "Overstaffed Ratio", solidHeader = TRUE, width = NULL, status = "primary",
+                             plotOutput("overstaffed_chart"),tags$head(tags$style("#overstaffed_chart{height:25vh !important;}")))),
+                  column(width = 6,
+                         box(title = "Lessons Distribution", solidHeader = TRUE, width = NULL, status = "primary",
+                             plotOutput("lessons_d_chart"),tags$head(tags$style("#lessons_d_chart{height:25vh !important;}"))))
                 )
-      ),
-      tabItem("capacity",
-              #KPI Row
-              fluidRow(
-                valueBoxOutput("lessons_instructor"),
-                valueBoxOutput("days_overstaffed"),
-                valueBoxOutput("avg_lessons_day")
-              ),
-              
-              #Charts Row
-              fluidRow(
-                column(width = 6,
-                       box(title = "Lesson Trends", solidHeader = TRUE, width = NULL, status = "primary",
-                           plotOutput("lessons_chart"),tags$head(tags$style("#lessons_chart{height:25vh !important;}")))),
-                column(width = 6,
-                       box(title = "Lessons per Instructor", solidHeader = TRUE, width = NULL, status = "primary",
-                           plotOutput("lessons_i_chart"),tags$head(tags$style("#lessons_i_chart{height:25vh !important;}"))))),
-              fluidRow(
-                column(width = 6,
-                       box(title = "Overstaffed Ratio", solidHeader = TRUE, width = NULL, status = "primary",
-                           plotOutput("overstaffed_chart"),tags$head(tags$style("#overstaffed_chart{height:25vh !important;}")))),
-                column(width = 6,
-                       box(title = "Lessons Distribution", solidHeader = TRUE, width = NULL, status = "primary",
-                           plotOutput("lessons_d_chart"),tags$head(tags$style("#lessons_d_chart{height:25vh !important;}"))))
-              )
-      ),
-      tabItem("weather"),
-      tabItem("scenario")
+        ),
+        tabItem("weather"),
+        tabItem("scenario")
+      )
     )
   )
 )
@@ -176,12 +175,12 @@ server <- function(input, output) {
              subtitle = "Total Profit",icon = icon("usd"),color = "green")})  
   
   output$avg_profit_lesson <- renderValueBox({
-    valueBox(value = paste("$", format(summ_data()$profit/summ_data()$lessons, 
+    valueBox(value = paste("$", format(round(summ_data()$profit/summ_data()$lessons,0), 
                                         format="d", big.mark=",")),
              subtitle = "Avg. Profit/Lesson",icon = icon("ticket"),color = "aqua")})
   
   output$avg_profit_instructor <- renderValueBox({
-    valueBox(value = paste("$", format(summ_data()$profit/summ_data()$total_inst,
+    valueBox(value = paste("$", format(round(summ_data()$profit/summ_data()$total_inst,1),
                                         format="d", big.mark=",")),
              subtitle = "Avg. Profit/Staff",icon = icon("street-view"),color = "purple")})  
   
@@ -202,15 +201,15 @@ server <- function(input, output) {
   # -----------------------------------  Capacity Tab ----------------------------------
   # ----------------------------------- Cpacity KPIs ----------------------------------
   output$lessons_instructor <- renderValueBox({
-    valueBox(value = paste(toString(summ_data()$lessons/summ_data()$total_inst), '%'),
+    valueBox(value = toString(round(mean(summ_data()$lessons/summ_data()$total_inst),2)),
              subtitle = "Lessons/Instructor",icon = icon("graduation-cap"),color = "green")})  
   
   output$days_overstaffed <- renderValueBox({
-    valueBox(value = paste(toString(round(summ_data()$days_ostaffed/summ_data()$days_open*100,0)), '%'),
+    valueBox(value = paste(toString(round(mean(summ_data()$days_ostaffed/summ_data()$days_open*100),0)), '%'),
              subtitle = "% Days Overstaffed",icon = icon("user-plus"),color = "aqua")})
   
   output$avg_lessons_day <- renderValueBox({
-    valueBox(value = summ_data()$lessons/summ_data()$days_open,
+    valueBox(value = round(summ_data()$lessons/summ_data()$days_open,1),
              subtitle = "Lessons per Day",icon = icon("street-view"),color = "purple")})  
   
   # ----------------------------------- Capacity Charts ----------------------------------
@@ -232,7 +231,7 @@ server <- function(input, output) {
     x <- t(season_info())
     colnames(x) <- c('Season Info')
     x
-  }, options = list(scrollX = TRUE))
+  })
 }
 
 # Run the application 
